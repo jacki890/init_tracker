e8a5c4679e1bc39c8feb20419543f61b7e6ed71a
Jacki Keys <jacki@edgecase.com>
2011-04-15 16:05:10 -0400

    replaced dice roll with user input

--- character.rb
+++ character.rb
@@ -2,7 +2,17 @@ class Character
   attr_accessor :roll, :name
   def initialize(name)
     self.name = name
-    self.roll = rand(20) + 1
+    self.roll = input_dice_roll
+  end
+
+  def input_dice_roll
+    puts "Please type in the result of the d10 dice roll (number from 1 - 10)"
+    dice_input = gets.chomp.to_i
+    while dice_input < 1 || dice_input > 10
+      puts "Please type a number between 1 and 10"
+      dice_input = gets.chomp.to_i
+    end
+    dice_input
   end
 
   def <=>(o)
--- init_tracker.rb
+++ init_tracker.rb
@@ -4,17 +4,8 @@ class InitTracker
 
   def initialize
     @characters = File.new('./player_characters.txt', "r").readlines.collect do |character|
-      Character.new(character.chomp)
-    # lines = @characters.collect do |chars|
-    #   "#{dice_roll} #{chars}\\n"
-    # end.sort
-
-    end.sort.reverse
+      Character.new(character.chomp) end.sort.reverse
   end
-
-  # def dice_roll(sides = 6)
-  #   rand(sides) + 1
-  # end
 
   def write_file
     File.open('./order.txt', "w") do |init_file|
@@ -22,5 +13,6 @@ class InitTracker
         init_file.write("#{character.padded_roll} #{character.name}\n")
       end
     end
+    puts(File.open('./order.txt', "r").readlines)
   end
 end
