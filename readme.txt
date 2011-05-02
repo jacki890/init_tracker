650987f2cc8bf1013c648478656bc1be4850485d
Jacki Keys <jacki@edgecase.com>
2011-05-02 10:43:08 -0400

    Added readme.txt to repo.

---
+++ readme.txt
@@ -0,0 +1,10 @@
+I want to create an initiative tracking program to use with
+D&D style games. The main function is for the DM to put PC and
+NPC names into a text file, manually enter dice roll numbers
+for each character, then output the character's names depending
+the initiative rules.
+
+I would eventually like it to include rules from various versions
+of D&D and AD&D with the ability to switch between them. I also
+plan to include hit point tracking and so forth to make things
+a lot easier for the DM to keep track of.
--- test_character.rb
+++ test_character.rb
@@ -1,13 +1,34 @@
 require 'test/unit'
 require 'character'
 
+@@gets_returns = ""
+class Character
+  def gets; @@gets_returns; end
+  def puts(x); end
+end
+
 class TestCharacter < Test::Unit::TestCase
   def test_accepts_name
+    @@gets_returns = ""
     char = Character.new "steve"
     assert_equal "steve", char.name
   end
 
+  def test_input_is_number_between_1_and_10
+    @@gets_returns = "7"
+    char = Character.new "steve"
+    assert_equal 7, char.input_dice_roll
+  end
+
+  def test_input_is_empty_string
+    @@gets_returns = ""
+    char = Character.new "steve"
+    assert char.input_dice_roll < 11, "was not less than 11"
+    assert char.input_dice_roll > 0, "was not greater than 0"
+  end
+
   def test_sortable_by_roll
+    @@gets_returns = ""
     chars = %w{steve bob joe marley mike}.collect{|name| Character.new(name) }
     sorted_chars = chars.sort
     rolls = sorted_chars.collect{|character| character.roll }
